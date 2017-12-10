bfcb4509cf5cf0914675e5193f019cb3ad69df78
Ricky <ricky.whitaker@undeadcodersociety.com>
2017-12-10 17:00:59 -0600

    got little controller working

--- application/app.py
+++ application/app.py
@@ -9,16 +9,33 @@ from os import makedirs, path
 def create_app():
     app = Flask(import_name=__name__)
 
+    submssion_choices = {'0': 'White',
+                         '1': 'Red',
+                         '2': 'Green',
+                         '3': 'Blue',
+                         '4': 'Orange',
+                         '5': 'Turquoise',
+                         '6': 'Purple',
+                         'Ok': 'Off',
+                         'Pound': 'Brightness Up',
+                         'Star': 'Brightness Down',
+                         'Right': 'Speed Up',
+                         'Left': 'Speed Down',
+                         'Up': 'Mode Up',
+                         'Down': 'Mode Down',
+                         }
+
     @app.route('/', methods=['GET', 'POST'])
     def homepage():
         if request.method.lower() == 'post':
-            if request.form.get('mode'):
-                color = request.form['mode']
+            if request.form.get('submission_choice'):
+                color = request.form['submission_choice']
                 if not path.exists("/home/pi/LED_Project"):
                     makedirs('/home/pi/LED_Project')
                 with open('/home/pi/LED_Project/input.json', 'w') as outfile:
                     outfile.write(dumps({'input': color}))
-        return render_template('homepage.html')
+        return render_template('homepage.html',
+                               submission_choices=dumps(submssion_choices))
 
     return app
 

e76c5bcb2cb31892b477d9b8390f29bdb052ce16
Ricky <ricky.whitaker@undeadcodersociety.com>
2017-12-10 13:20:15 -0600

    page rendering and expansion of modes after successful first test

--- application/app.py
+++ application/app.py
@@ -3,6 +3,7 @@
 
 from flask import (Flask, render_template, request)
 from json import dumps
+from os import makedirs, path
 
 
 def create_app():
@@ -11,10 +12,12 @@ def create_app():
     @app.route('/', methods=['GET', 'POST'])
     def homepage():
         if request.method.lower() == 'post':
-            if request.form.get('color'):
-                color = request.form['color']
+            if request.form.get('mode'):
+                color = request.form['mode']
+                if not path.exists("/home/pi/LED_Project"):
+                    makedirs('/home/pi/LED_Project')
                 with open('/home/pi/LED_Project/input.json', 'w') as outfile:
-                    outfile.write(dumps({'color': color}))
+                    outfile.write(dumps({'input': color}))
         return render_template('homepage.html')
 
     return app
